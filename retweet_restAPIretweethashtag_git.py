0c4d47da909ec0785629e6021658a1646e4a4d11
pragy30 <pragyachaube@gmail.com>
2021-05-31 21:47:36 +0530

    Update retweet_restAPIretweethashtag_git.py
    removed the logger verification. removed the api verification bit.

--- retweet_restAPIretweethashtag_git.py
+++ retweet_restAPIretweethashtag_git.py
@@ -6,29 +6,16 @@ import logging
 #from config import create_api
 import json
 import os
+import time
 
-logger = logging.getLogger()
+consumer_key = os.getenv("CONSUMER_KEY")
+consumer_secret = os.getenv("CONSUMER_SECRET")
+access_token = os.getenv("ACCESS_TOKEN")
+access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
-def create_api():
-    consumer_key = os.getenv("CONSUMER_KEY")
-    consumer_secret = os.getenv("CONSUMER_SECRET")
-    access_token = os.getenv("ACCESS_TOKEN")
-    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
-
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth, wait_on_rate_limit=True, 
-        wait_on_rate_limit_notify=True)
-    try:
-        api.verify_credentials()
-    except Exception as e:
-        logger.error("Error creating API", exc_info=True)
-        raise e
-    logger.info("API created")
-    return api
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger()
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 	
 searchQuery = '#sciencepolicy OR #STIP2021 OR #scipol OR #scienceadvice OR #sciencediplomacy OR #scidip OR #techdiplomacy OR #techplomacy OR #ScienceTechnologyStudies OR #researchandinnovation OR #IndianScience OR #scienceethics -filter:retweets AND -filter:replies'
 
